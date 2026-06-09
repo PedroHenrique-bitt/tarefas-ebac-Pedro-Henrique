30b2bef01a8589614f9575f2ef13836dc7ddb0ca
PedroHenrique-bitt <condporuus@gmail.com>
2026-06-09 19:09:03 -0300

    Modulo6

---
+++ tarefas-ebac-Pedro-Henrique/Modulo6/Rotina.java
@@ -0,0 +1,11 @@
+        1. Acordo assim que o despertador toca
+        2. Levanto da minha cama
+        3. Tomo agua
+        4. Vou ao banheiro tomar banho
+            1. Se estiver frio não tomo banho
+        5. Escovo meus dentes
+        6. Volto ao quarto pra me trocar
+        7. Vou a cozinha
+        8. Tomar café
+            1. Se eu estiver atrasado
+            2. Não tomo o café

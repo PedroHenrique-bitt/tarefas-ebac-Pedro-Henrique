2aa67a659bb071577290d662dfec8374d943275b
PedroHenrique-bitt <condporuus@gmail.com>
2026-06-09 19:19:30 -0300

    Modulo7

---
+++ tarefas-ebac-Pedro-Henrique/Modulo7/Geladeira.java
@@ -0,0 +1,24 @@
+package Geladeira;
+
+public class Geladeira {
+	
+	private int marca;
+	
+	private String quantidade;
+	
+	public int getMarca() {
+		return marca;
+}
+//foi uma tentativa de colocar em pratica a aula do modulo7	
+	public void setMarca(int marca) {
+		this.marca = marca; 
+	}
+
+	public String getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+}
